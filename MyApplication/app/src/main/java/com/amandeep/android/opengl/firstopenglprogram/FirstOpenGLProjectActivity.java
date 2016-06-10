cee9b23b2873ad534317f1646da26ad4c67af076
Amandeep Chawla <amandeep@adobe.com>
2016-06-10 20:49:50 +0530

    Initial App with Empty Activity

---
+++ MyApplication/app/src/main/java/com/amandeep/android/opengl/firstopenglprogram/FirstOpenGLProjectActivity.java
@@ -0,0 +1,13 @@
+package com.amandeep.android.opengl.firstopenglprogram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstOpenGLProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_open_glproject);
+    }
+}
